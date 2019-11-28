1b0cd984a4b9087340a59311ba8c11f193c8390b
Nikola Djordjevic <nikola.djordjevic04@gmail.com>
2019-11-28 18:15:14 +0100

    banka https

--- MasterProject/src/main/java/com/master/demo/ConnectorConfig.java
+++ MasterProject/src/main/java/com/master/demo/ConnectorConfig.java
@@ -6,12 +6,5 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ConnectorConfig {
 
-//  private Connector getHttpConnector() {
-//    Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-//    connector.setScheme("http");
-//    connector.setPort(8080);
-//    connector.setSecure(false);
-//    connector.setRedirectPort(8081);
-//    return connector;
-//  }
+//
 }
