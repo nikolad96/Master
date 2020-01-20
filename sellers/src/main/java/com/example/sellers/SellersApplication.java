8c03ffcb9b6cae8dc1dc58014eac9eff2627ba75
Milica Makaric <makaric.milica@uns.ac.rs>
2020-01-20 13:35:33 +0100

    application.properties changes

--- sellers/src/main/java/com/example/sellers/SellersApplication.java
+++ sellers/src/main/java/com/example/sellers/SellersApplication.java
@@ -1,8 +1,14 @@
 package com.example.sellers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import javax.annotation.PostConstruct;
+import java.util.Properties;
 
 @EnableEurekaClient
 @SpringBootApplication
@@ -10,6 +16,21 @@ public class SellersApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SellersApplication.class, args);
+    }
+
+    @Configuration
+    public class SSLConfig {
+
+        @Autowired
+        private Environment env;
+
+        @PostConstruct
+        private void configureSSL() {
+            Properties systemProps = System.getProperties();
+            systemProps.put("javax.net.ssl.trustStore", env.getProperty("trust.store"));
+            systemProps.put("javax.net.ssl.trustStorePassword",env.getProperty("trust.store.password"));
+            System.setProperties(systemProps);
+        }
     }
 
 }
