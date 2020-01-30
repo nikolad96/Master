11c3c2d6d5c3c709528b3390ad26e16a18f2fdd7
NikolaMal <33634609+NikolaMal@users.noreply.github.com>
2020-01-30 10:10:23 +0100

    hm

--- naucna-centrala/src/main/java/com/example/naucnacentrala/NaucnaCentralaApplication.java
+++ naucna-centrala/src/main/java/com/example/naucnacentrala/NaucnaCentralaApplication.java
@@ -40,19 +40,19 @@ public class NaucnaCentralaApplication {
         return registrationBean;
     }
 
-//    @Bean
-//    public FilterRegistrationBean corsFilter() {
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        CorsConfiguration config = new CorsConfiguration();
-//        config.setAllowCredentials(true);
-//        config.addAllowedOrigin("*");
-//        config.addAllowedHeader("*");
-//        config.addAllowedMethod("*");
-//        source.registerCorsConfiguration("/**", config);
-//        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
-//        bean.setOrder(0);
-//        return bean;
-//    }
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
+    }
     @Configuration
     public class SSLConfig {
 
